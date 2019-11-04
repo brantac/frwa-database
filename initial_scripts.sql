5a39df2871d512129891b2b8c337d088fa13ece4
Arthur Caldeira Brant <arthurcl.br@hotmail.com>
2019-11-03 22:19:21 -0300

    criado BD e tabelas iniciais

---
+++ initial_scripts.sql
@@ -0,0 +1,16 @@
+-- Criar banco de dados
+CREATE fra_table;
+
+-- Criar tabelas
+CREATE TABLE tb_users(
+);
+
+CREATE TABLE tb_food();
+
+CREATE TABLE tb_meal_history();
+
+CREATE TABLE tb_foods_meal();
+
+-- Inserir dados
+INSERTO INTO tb_users () 
+VALUES();
